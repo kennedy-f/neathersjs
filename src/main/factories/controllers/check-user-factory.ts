401f508a873ee9120495bf1da185a0378c12a956
Kennedy Silva <kennedy2000silva@hotmail.com>
2021-03-10 10:57:31 -0300

    commented unused import

--- src/main/factories/controllers/check-user-factory.ts
+++ src/main/factories/controllers/check-user-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../presentation/protocols';
 import { CheckUserByEmailController } from '../../../presentation/controllers';
 import { makeCheckUserByEmailController } from './check-user-by-email-factory';
-import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory';
+// import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'; fix bug 
 
 export const makeCheckUser = (): Controller => {
   const controller = new CheckUserByEmailController(
